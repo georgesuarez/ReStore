86cf879a8bfa4c166e0aa6316864671b90935098
georgesuarez <georgejsuarez@gmail.com>
2022-07-21 13:39:05 -0700

    End of section 4

--- client/src/app/features/catalog/ProductCard.tsx
+++ client/src/app/features/catalog/ProductCard.tsx
@@ -1,4 +1,5 @@
 import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 import { Product } from "../../models/product";
 
 interface Props {
@@ -33,8 +34,8 @@ export default function ProductCard({product}: Props) {
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
+                <Button size="small">Add to cart</Button>
+                <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
             </CardActions>
         </Card>
     )
